bdea3a5ab52a44c5a0a19c67edce569a9c9171e8
devvspaces <netrobepy@gmail.com>
2022-03-09 16:55:55 +0100

    update

--- setup_venv.py
+++ setup_venv.py
@@ -11,37 +11,26 @@ if process.returncode == 0:
     print(f"Successfully created virtual enviroment {env}")
 
     # Activate the virtual enviroment
-    process = subprocess.run(['source', f"{env}/bin/activate"], capture_output=True, text=True)
+    process = subprocess.run(['source', f"{env}/bin/activate"], shell=True)
 
     if process.returncode == 0:
-        print(f'Activating enviroment {env}')
+        print(f'Activated enviroment {env}')
 
         # Install requirements.txt
-        process = subprocess.run(f"source {env}/bin/activate", capture_output=True, shell=True)
+        process = subprocess.run(['pip install -r requirement.txt'], capture_output=True, text=True)
 
         if process.returncode == 0:
-            print('Installing packages in requirements.txt')
+            print('Installed packages in requirements.txt')
 
-            # Install requirements.txt
-            process = subprocess.run(['pip install -r requirement.txt'], capture_output=True, text=True)
-
-            if process.returncode == 0:
-                print('Installed packages in requirements.txt')
-
-                print(process.stdout)
-            
-            else:
-                print('Error while installing packages')
-                print(process.stderr)
+            print(process.stdout)
         
         else:
-            print('Error while activating virtual enviroment')
+            print('Error while installing packages')
             print(process.stderr)
-    
+        
     else:
         print('Error while activating virtual enviroment')
         print(process.stderr)
-
 
 else:
     print('Error while creating virtual enviroment')
