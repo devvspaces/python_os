d5aa177d12466ee3f2be4581a210657b03497ceb
devvspaces <netrobepy@gmail.com>
2022-03-09 16:44:20 +0100

    update

---
+++ setup_venv.py
@@ -0,0 +1,53 @@
+import subprocess, sys
+
+
+# get the name for the virtual enviroment
+env = sys.argv[0]
+
+# Create the virtual enviroment
+process = subprocess.run(['virtualenv', env], capture_output=True, text=True)
+
+if process.returncode == 0:
+    print(f"Successfully created virtual enviroment {env}")
+
+    # Activate the virtual enviroment
+    process = subprocess.run(['source', f"{env}/bin/activate"], capture_output=True, text=True)
+
+    if process.returncode == 0:
+        print(f'Activating enviroment {env}')
+
+        # Install requirements.txt
+        process = subprocess.run(['source', f"{env}/bin/activate"], capture_output=True, text=True)
+
+        if process.returncode == 0:
+            print('Installing packages in requirements.txt')
+
+            # Install requirements.txt
+            process = subprocess.run(['pip install -r requirement.txt'], capture_output=True, text=True)
+
+            if process.returncode == 0:
+                print('Installed packages in requirements.txt')
+
+                print(process.stdout)
+            
+            else:
+                print('Error while installing packages')
+                print(process.stderr)
+        
+        else:
+            print('Error while activating virtual enviroment')
+            print(process.stderr)
+    
+    else:
+        print('Error while activating virtual enviroment')
+        print(process.stderr)
+
+
+else:
+    print('Error while creating virtual enviroment')
+    print(process.stderr)
+
+
+
+
+
